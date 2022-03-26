8cb9b8d846196377df84e3b5babc437988986430
Enrique Lechuga <kratoskike@gmai.com>
2022-03-26 17:25:31 +0100

    test angular

---
+++ Spring_OW/src/main/java/com/example/Spring_OW/model/Usuario.java
@@ -0,0 +1,5 @@
+package com.example.Spring_OW.model;
+
+public class Usuario {
+
+}
---
+++ Spring_OW/src/test/java/com/example/Spring_OW/SpringOwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_OW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
